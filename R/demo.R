ca5461c2b51600a302cc660f3dbc0d0a06b4dfe1
Geoff LaFlair <gtlaflair@gmail.com>
2018-12-07 11:07:31 -1000

    updates!

--- R/demo.R
+++ R/demo.R
@@ -1,19 +1,14 @@
 ## @knitr loadps
 
-library(ggplot2)
-library(readr)
-library(dplyr)
+library(tidyverse)
 library(here)
 library(haven)
 
 ## @knitr readdata
 
 toth <- here::here('data/Toth2008GJT.sav') %>%
-  read_spss()
-
-
-## @knitr knowdata
-
+  read_spss() %>%
+  mutate(Group = factor(Group))
 
 
 ## @knitr boxplots
@@ -22,10 +17,11 @@ toth <- here::here('data/Toth2008GJT.sav') %>%
 # x = respondent_wall_type, y = rooms
 # add jitter, width = .20, alpha = .60
 
-box <- ggplot(data = int_plot, aes(x = respondent_wall_type,
-                                   y = rooms)) +
+box <- ggplot(toth, aes(x = Group, y = GJTpretest, fill = Group)) +
   geom_boxplot() +
-  geom_jitter(width = .20, alpha = .60)
+  geom_jitter(width = .20, alpha = .60) +
+  scale_fill_viridis_d(alpha = .30) 
+
 
 
 ## @knitr scatterplots
@@ -35,82 +31,22 @@ box <- ggplot(data = int_plot, aes(x = respondent_wall_type,
 # start with geom_point()
 # iteratively add geom_jitter(alpha = 0.5, aes(color = village))
 
-scatter <- ggplot(int_plot, aes(x = no_membrs, 
-                                y = number_items,
-                                color = village)) +
+scatter <- ggplot(toth, aes(x = GJTpretest, y = GJTposttest, color = Group)) +
   geom_point() +
-  geom_jitter(alpha = .5) +
-  ylab("Number of Items") +
-  xlab("Number of People in Family") +
-  ggtitle("Number of Items by People") +
   scale_color_viridis_d(option = 'inferno')
 
-## @knitr barplots
+## @knitr tables
 
-# name the object bar
-# use var respondent_wall_type for x, fill, and color; add scale_fill_v & scale_color_v
-# add theme(legend.position = 'none')
+toth_long <- toth %>%
+  gather(key = Test, value = Score, -subject, -Group)
 
-bar <- ggplot(int_plot, aes(x = respondent_wall_type, 
-                            fill = respondent_wall_type,
-                            color = respondent_wall_type)) +
-  geom_bar() +
-  scale_color_viridis_d() +
-  scale_fill_viridis_d()
-bar
-## @knitr faceting
+toth_sum <- toth_long %>%
+  group_by(Group, Test) %>%
+  summarise(N = n(),
+            Mean = mean(Score),
+            SD = sd(Score))
 
-# facet by village
-# x = respondent_wall_type, fill = respondent_wall_type
-# add dodge to second plot, proportions to third, use viridis for all
+toth_pre <- filter(toth_sum, Test %in% 'GJTpretest')
+toth_pst <- filter(toth_sum, Test %in% 'GJTposttest')
+toth_del <- filter(toth_sum, Test %in% 'GJTdelayed')
 
-bar_vil <- ggplot(int_plot, aes(x = respondent_wall_type, 
-                                fill = respondent_wall_type,
-                                color = respondent_wall_type)) +
-  geom_bar() +
-  scale_color_viridis_d() +
-  scale_fill_viridis_d() +
-  facet_wrap(~ village)
-
-# show proportions
-# filter(respondent_wall_type != "cement") %>%
-#   count(village, respondent_wall_type) %>%
-#   group_by(village) %>%
-#   mutate(percent = n / sum(n)) %>%
-#   ungroup()
-
-# facet by items
-
-percent_items <- int_plot %>%
-  # select variable needed and turn dataframe into long format
-  gather(items, items_owned_logical, bicycle:no_listed_items)%>%
-  # keep observations in which the items are owned
-  filter(items_owned_logical) %>%
-  # count the items by village; this creates the n variable
-  count(items, village) %>%
-  # add a column with the number of people in each village
-  mutate(people_in_village = case_when(village == "Chirodzo" ~ 39,
-                                       village == "God" ~ 43,
-                                       village == "Ruaca" ~ 49)) %>%
-  # make a percentage column
-  mutate(percent = n / people_in_village)
-
-# add themes
-
-# show theme_minimal first
-theme_evergreen_s <- theme_minimal() + theme(axis.ticks = element_blank(),
-                                             panel.grid = element_blank())
-
-theme_set(theme_evergreen_s)
-
-## @knitr timeseries
-
-# air <- economics %>%
-#   ggplot(., aes(x = date, y = pop)) +
-#   geom_line()
-# 
-# air_two <- economics %>%
-#   select(date, psavert, uempmed) %>%
-#   gather(key, value, -date) %>%
-#   ggplot(., aes(x = date, y = value, color = key)) +
-#   geom_line()
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # uh-ds-pres
 Reproducible presentations in RStudio
+
+## Packages
+
+`install.packages(c('knitr', 'haven', 'leaflet', 'kableExtra', 'tidyverse', 'wesanderson'))`        
+
+`devtools::install_github('yihue/xaringan')`
